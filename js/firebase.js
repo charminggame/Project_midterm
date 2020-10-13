b18a7340a65bbf412c6db2d1de4dd4372458d164
charminggame <62237314+charminggame@users.noreply.github.com>
2020-10-13 14:39:20 +0700

    Revert "Revert "-0.01""
    This reverts commit 7af68ef00b179f9077089319673ac41cbe6d5a08.

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBo28YzaTBNmQmhQJ9z5PXXekpyAD41vMw",
+    authDomain: "midterm-project-db575.firebaseapp.com",
+    databaseURL: "https://midterm-project-db575.firebaseio.com",
+    projectId: "midterm-project-db575",
+    storageBucket: "midterm-project-db575.appspot.com",
+    messagingSenderId: "782407649227",
+    appId: "1:782407649227:web:3bc69f188c5290e4ef584e",
+    measurementId: "G-49YL7M5GS9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
