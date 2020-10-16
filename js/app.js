61f674643bc31555d2204b51fc75fd7a7d011430
tontal1705 <63768989+tontal1705@users.noreply.github.com>
2020-10-16 15:34:17 +0700

    add detail movie

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 document.addEventListener('init', function(event) {
-  var page = event.target;
+    var page = event.target;
 
-  if (page.id === 'page1') {
-    page.querySelector('#m1').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('views/detail.html', {data: {title: 'Page 2'}});
-    };
-  } else if (page.id === 'page2') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
+    if (page.id === 'page1') {
+        page.querySelector('#a1').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('views/Adventure1.html', { data: { title: 'Page 2' } });
+        };
+
+        page.querySelector('#c1').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('views/Comedy1.html', { data: { title: 'Page 2' } });
+        };
+
+        page.querySelector('#f1').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('views/Family1.html', { data: { title: 'Page 2' } });
+        };
+    } else if (page.id === 'page2') {
+        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    }
 });
